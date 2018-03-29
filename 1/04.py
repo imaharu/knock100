cf4c728a5dc49ef07ecf1d41a2fd0764753f6092
imaharu <light1200out@gmail.com>
2018-03-29 17:36:50 +0900

    Done 04

--- 1/04.py
+++ 1/04.py
@@ -1,3 +1,11 @@
 # 04. 元素記号
 element = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+ans = {}
 clear_element = element.replace(",", "").replace(".", "").split(" ")
+branch = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+for i in range(len(clear_element)):
+    if i in branch:
+        ans[i] = clear_element[i][0:1:1]
+    else:
+        ans[i] = clear_element[i][0:2:1]
+print(ans)
