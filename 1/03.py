6d61d50275468ee793c02ebc810c93b1701b1c73
imaharu <light1200out@gmail.com>
2018-03-29 17:19:58 +0900

    Update 03

--- 1/03.py
+++ 1/03.py
@@ -1,6 +1,7 @@
 # 03. 円周率
 pi = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 clear_pi = pi.replace(",", "").replace(".", "").split(" ")
-
+pi_list = []
 for i in range(len(clear_pi)):
-    print(len(clear_pi[i]))
+    pi_list.append(len(clear_pi[i]))
+print(pi_list)
---
+++ 1/04.py
@@ -0,0 +1,3 @@
+# 04. 元素記号
+element = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+clear_element = element.replace(",", "").replace(".", "").split(" ")
