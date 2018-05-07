d697afdfc544da2f55a542a5e2f68b6ab5a9ffa9
imaharu <light1200out@gmail.com>
2018-05-07 20:38:59 +0900

    Fix 22

--- 3/22.py
+++ 3/22.py
@@ -1,6 +1,25 @@
+
+        #m = re.search(r'Category:(?P<category>.+?)((\])|(\|))',line)
+
+
+import json
 import re
-with open('article.txt') as f:
-    for line in f:
-        m = re.search(r'Category:(?P<category>.+?)((\])|(\|))',line)
-        if m:
-            print(m.group("category"))
+fname = 'jawiki-country.json'
+def UK():
+
+    with open(fname, 'r') as data_file:
+        for line in data_file:
+            data_json = json.loads(line)
+            if data_json['title'] == 'イギリス':
+                return data_json['text']
+
+    raise ValueError('イギリスの記事が見つからない')
+
+pattern = re.compile(r'^\[\[Category:(?P<category>.+?)\]\]$',re.MULTILINE + re.DOTALL)
+contents = pattern.findall(UK())
+
+for content in contents:
+    content = re.sub(r"\|\*","",content)
+    pattern2 = re.compile(r'\|',re.MULTILINE + re.DOTALL)
+    for content in pattern2.split(content):
+        print(content)
